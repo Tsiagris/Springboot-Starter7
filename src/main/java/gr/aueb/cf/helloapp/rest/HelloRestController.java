63473e55aa79f05c907930b05d2720d2c7662ebf
Anastassios Siagkris <asiagris2011@gmail.com>
2025-05-22 21:08:10 +0300

    chore: fix typo in code

--- src/main/java/gr/aueb/cf/helloapp/rest/HelloRestController.java
+++ src/main/java/gr/aueb/cf/helloapp/rest/HelloRestController.java
@@ -1,32 +1,37 @@
 package gr.aueb.cf.helloapp.rest;
 
+import gr.aueb.cf.helloapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.helloapp.dto.TeacherReadOnlyDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.transform.OutputKeys;
 import java.util.Objects;
 
-@Controller
+@RestController
 @RequestMapping("/hello")
 public class HelloRestController {
 
-
     @GetMapping("/coding-factory")
     public String sayHello() {
-        return "Hello Coding Factory !";
+        return "Hello Coding Factory!";
     }
 
+    @GetMapping("/teachers/{id}")
     public ResponseEntity<TeacherReadOnlyDTO> getOneTeacher(@PathVariable("id") Long id) {
-        TeacherReadOnlyDTO readOnlyDTO = new TeacherReadOnlyDTO(1L, "Αθανασιος", "Ανδρουτσος");
+        TeacherReadOnlyDTO readOnlyDTO = new TeacherReadOnlyDTO(1L, "Αθανάσιος", "Ανδρούτσος");
 
-        if(Objects.equals(id, 1L)) {
+        if (Objects.equals(id, 1L)) {
             return new ResponseEntity<>(readOnlyDTO, HttpStatus.OK);
         } else {
-            return new  ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping("/teachers")
+    public ResponseEntity<TeacherReadOnlyDTO> addTeacher(@RequestBody TeacherInsertDTO insertDTO) {
+        TeacherReadOnlyDTO readOnlyDTO = new TeacherReadOnlyDTO(1L, insertDTO.firstname(), insertDTO.lastname());
+        return new ResponseEntity<>(readOnlyDTO, HttpStatus.CREATED);
     }
 }
